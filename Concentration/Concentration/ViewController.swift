3b81358565858b45d2e1dd3c9bc6f7c65eb56841
GXGMAdhaTTer <chengxinglugxg@gmail.com>
2019-05-08 20:52:58 +0800

    Finished

--- Concentration/Concentration/ViewController.swift
+++ Concentration/Concentration/ViewController.swift
@@ -10,20 +10,35 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    @IBAction func touchCard(_ sender: UIButton) {
-        print("agh! A ghost!")
-        flipCard(withEmoji: "👻", on: sender)
+    var flipCount = 0 {
+        didSet {
+            flipCountLabel.text = "Flips: \(flipCount)"
+        }
     }
     
-    @IBAction func touchSecondCard(_ sender: UIButton) {
-        flipCard(withEmoji: "🎃", on: sender)
+    @IBOutlet weak var flipCountLabel: UILabel!
+    
+    @IBOutlet var cardButtons: [UIButton]!
+    
+    var emojiChoices = ["🎃", "👻", "🎃", "👻"]
+    
+    @IBAction func touchCard(_ sender: UIButton) {
+//        print("agh! A ghost!")
+        flipCount += 1
+        if let cardNumber = cardButtons.index(of: sender) {
+            flipCard(withEmoji: emojiChoices[cardNumber], on: sender)
+        } else {
+            print("chosen card was not in cardButtons")
+        }
+        
     }
+
     
     func flipCard(withEmoji emoji: String, on button: UIButton) {
         if button.currentTitle == emoji {
             button.setTitle("", for: UIControl.State.normal)
             button.backgroundColor = #colorLiteral(red: 1, green: 0.5763723254, blue: 0, alpha: 1)
-            print("flipCard(withEmoji: %s) ", emoji)
+            print("flipCard(withEmoji: \(emoji) ")
         }else {
             button.setTitle(emoji, for: UIControl.State.normal)
             button.backgroundColor = #colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)
