b1ed3960844e68c2287b813e7e8878f575869510
GXGMAdhaTTer <chengxinglugxg@gmail.com>
2019-05-10 01:03:17 +0800

    eposide 4 finished

--- Concentration/Concentration/ViewController.swift
+++ Concentration/Concentration/ViewController.swift
@@ -67,7 +67,17 @@ class ViewController: UIViewController {
         return emoji[card.identifier] ?? "?"
     }
     
-    
-    
+}
+
+extension Int {
+    var arc4random: Int {
+        if self > 0 {
+            return Int(arc4random_uniform(UInt32(self)))
+        } else if self < 0 {
+            return -Int(arc4random_uniform(UInt32(abs(self))))
+        } else {
+            return 0
+        }
+    }
 }
 
